d1f68e7e4fdb22ce0a3899a2218b511ff9ab9414
ayadi1 <ayadiossama44@gmail.com>
2022-03-05 22:03:35 +0100

    apply style for main content

--- components/Main/subComponents/Form.js
+++ components/Main/subComponents/Form.js
@@ -3,24 +3,33 @@ export default function Form() {
   return (
     <div className={styles.mainForm}>
       <div className={styles.form}>
-        <input type="text" placeholder="Shorten a link here..." />
-        <button>Shorten It!</button>
+        <div className={styles.inputs}>
+          <input type="text" placeholder="Shorten a link here..." />
+          <button>Shorten It!</button>
+        </div>
+        <div className={styles.errMessage}>errMessage...</div>
       </div>
       <div className={styles.list}>
         <div className={styles.listItem}>
           <p className={styles.mainUrl}>www.google.com</p>
-          <p className={styles.shortUrl}>go.gle</p>
-          <button>copy</button>
+          <div className={styles.actions}>
+            <p className={styles.shortUrl}>go.gle</p>
+            <button>copy</button>
+          </div>
         </div>
         <div className={styles.listItem}>
           <p className={styles.mainUrl}>www.google.com</p>
-          <p className={styles.shortUrl}>go.gle</p>
-          <button>copy</button>
+          <div className={styles.actions}>
+            <p className={styles.shortUrl}>go.gle</p>
+            <button>copy</button>
+          </div>
         </div>
         <div className={styles.listItem}>
           <p className={styles.mainUrl}>www.google.com</p>
-          <p className={styles.shortUrl}>go.gle</p>
-          <button>copy</button>
+          <div className={styles.actions}>
+            <p className={styles.shortUrl}>go.gle</p>
+            <button>copy</button>
+          </div>
         </div>
       </div>
     </div>
